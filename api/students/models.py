7f644d42f16097edb354267717d851bf35e21d6a
Vinícius Dias <vyvy292004@gmail.com>
2025-08-28 17:24:06 -0300

    feat: adds student model

---
+++ api/students/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+SHIFT_CHOICES = (
+    ("M", "Morning"),
+    ("A", "Afternoon"),
+    ("E", "Evening"),
+)
+
+UNIVERSITY_CHOICES = (
+    ("UESPI", "Universidade Estadual do Piauí"),
+    ("CHRISFAPI", "Christus Faculdade do Piauí"),
+    ("IFPI", "Instituto Federal do Piauí"),
+    ("ETC", "Outro"),
+)
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    phone = models.CharField(max_length=11, null=False, blank=False)
+    registration_date = models.DateField(auto_now_add=True)
+    class_shift = models.CharField(choices=SHIFT_CHOICES, blank=False, null=False)
+    university = models.CharField(choices=UNIVERSITY_CHOICES)
