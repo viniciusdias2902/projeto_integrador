ce4a5bac801ef8e7c9717cf861d70bdc891a7241
Vinícius Dias <vyvy292004@gmail.com>
2025-08-28 18:23:45 -0300

    adds driver model

--- api/drivers/models.py
+++ api/drivers/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from constants import SHIFT_CHOICES
 
-# Create your models here.
+
+class Driver(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=11)
+    shift = models.CharField(choices=SHIFT_CHOICES)
