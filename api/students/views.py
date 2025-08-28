92934138e29484fffab573c01f6a287997f72a07
Vinícius Dias <vyvy292004@gmail.com>
2025-08-28 17:45:25 -0300

    feat: adds students views to the API

--- api/students/views.py
+++ api/students/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+from .models import Student
+from .serializers import StudentSerializer, StudentCreateSerializer
 
-# Create your views here.
+
+class StudentListCreateView(ListCreateAPIView):
+    queryset = Student.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return StudentCreateSerializer
+        return StudentSerializer
+
+
+class StudentRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
