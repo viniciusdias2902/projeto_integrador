0bfec1b2604b92aef168d3dfdbf7865d89d99ec9
Vinícius Dias <vyvy292004@gmail.com>
2025-08-28 19:14:43 -0300

    gives permission for registration

--- api/drivers/views.py
+++ api/drivers/views.py
@@ -1,18 +1,22 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Driver
 from .serializers import DriverSerializer, DriverCreateSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from common.permissions import GlobalDefaultPermission
 
 
 class DriverListCreateView(ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Driver.objects.all()
 
     def get_serializer_class(self):
         if self.request.method == "POST":
             return DriverCreateSerializer
         return DriverSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [AllowAny()]
+        return [IsAuthenticated(), GlobalDefaultPermission()]
 
 
 class DriverRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
--- api/students/views.py
+++ api/students/views.py
@@ -1,18 +1,22 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Student
 from .serializers import StudentSerializer, StudentCreateSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from common.permissions import GlobalDefaultPermission
 
 
 class StudentListCreateView(ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GlobalDefaultPermission)
     queryset = Student.objects.all()
 
     def get_serializer_class(self):
         if self.request.method == "POST":
             return StudentCreateSerializer
         return StudentSerializer
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [AllowAny()]
+        return [IsAuthenticated(), GlobalDefaultPermission()]
 
 
 class StudentRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
