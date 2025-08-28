08d4d1173ae90cbb6cc3a3a565e4de1cd12f940c
Vinícius Dias <vyvy292004@gmail.com>
2025-08-28 18:55:20 -0300

    feat: adds authentication

--- api/app/urls.py
+++ api/app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/", include("authentication.urls")),
     path("api/v1/", include("students.urls")),
     path("api/v1/", include("drivers.urls")),
 ]
