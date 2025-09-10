ff883a5d20a7b43369f1ea38474af4eae863973b
Vinícius Dias <vyvy292004@gmail.com>
2025-09-10 05:08:47 -0300

    refactor: simplify vote and poll permissions

--- api/common/permissions.py
+++ api/common/permissions.py
@@ -4,7 +4,6 @@ from rest_framework import permissions
 class GlobalDefaultPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-
         model_permission_codename = self.__get_model_permission_codename(
             method=request.method,
             view=view,
--- api/poll/views.py
+++ api/poll/views.py
@@ -1,32 +1,25 @@
-# polls/views.py
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Poll, Vote
 from .serializers import PollSerializer, VoteSerializer
-from common.permissions import GlobalDefaultPermission
 from datetime import date, timedelta
 
 
 class PollListView(generics.ListAPIView):
     queryset = Poll.objects.all().order_by("date")
     serializer_class = PollSerializer
-
-    def get_permissions(self):
-        return [permissions.IsAuthenticated(), GlobalDefaultPermission()]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class PollDetailView(generics.RetrieveAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
-
-    def get_permissions(self):
-        return [permissions.IsAuthenticated(), GlobalDefaultPermission()]
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class PollBoardingListView(APIView):
-    def get_permissions(self):
-        return [permissions.IsAuthenticated(), GlobalDefaultPermission()]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request, pk):
         poll = generics.get_object_or_404(Poll, pk=pk)
@@ -41,7 +34,7 @@ class CreateTestPollsView(APIView):
     Only for testing purposes.
     """
 
-    permission_classes = [permissions.IsAuthenticated]  # or admin only
+    permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request):
         created_polls = []
@@ -60,11 +53,12 @@ class CreateTestPollsView(APIView):
         )
 
 
+# --- Vote Views --- #
+
+
 class VoteCreateView(generics.CreateAPIView):
     serializer_class = VoteSerializer
-
-    def get_permissions(self):
-        return [permissions.IsAuthenticated(), GlobalDefaultPermission()]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
         serializer.save(student=self.request.user.student)
@@ -72,9 +66,7 @@ class VoteCreateView(generics.CreateAPIView):
 
 class VoteListView(generics.ListAPIView):
     serializer_class = VoteSerializer
-
-    def get_permissions(self):
-        return [permissions.IsAuthenticated(), GlobalDefaultPermission()]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         return Vote.objects.filter(student=self.request.user.student)
