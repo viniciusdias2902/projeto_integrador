67d8d996679d77a5e29a8d0e553d4b79a4e931dc
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-11 03:57:18 -0300

    chore: alteradas configurações do banco de dados

--- api/app/settings.py
+++ api/app/settings.py
@@ -84,6 +84,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "projetointegrador",
+        "USER": "usuario",
+        "PASSWORD": "senha123",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
