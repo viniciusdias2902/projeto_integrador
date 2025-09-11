71f4ecf04895196719f3cf1ae05298afc9d1e532
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-11 03:58:02 -0300

    fix: removidas configuracoes antigas do banco sqlite

--- api/app/settings.py
+++ api/app/settings.py
@@ -77,12 +77,6 @@ WSGI_APPLICATION = "app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 DATABASES = {
     "default": {
