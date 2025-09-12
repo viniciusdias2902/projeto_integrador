33f964a079400bf5a76e47f56962c9b1a45a1b49
Vinícius Dias <vyvy292004@gmail.com>
2025-09-12 01:56:25 -0300

    feat: make migrations and adds a super user

--- api/venv/bin/activate.fish
+++ api/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/gabryel/projetos/projeto_integrador/api/venv
+set -gx VIRTUAL_ENV /home/vyvy/projects/projeto_integrador/api/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
