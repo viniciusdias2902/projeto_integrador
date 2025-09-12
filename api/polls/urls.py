ec822462cea6d869f0715fae6ded40f961677c0e
Vinícius Dias <vyvy292004@gmail.com>
2025-09-12 13:57:32 -0300

    refactor: remove unnecessary url routes

--- api/polls/urls.py
+++ api/polls/urls.py
@@ -23,7 +23,4 @@ urlpatterns = [
     ),
     path("votes/", VoteListView.as_view(), name="vote-list"),
     path("votes/create/", VoteCreateView.as_view(), name="vote-create"),
-    path(
-        "polls/<int:pk>/boarding/", PollBoardingListView.as_view(), name="poll-boarding"
-    ),
 ]
