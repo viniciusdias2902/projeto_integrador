2dda7d5d835a06cf765d63766a334fc1fe1d51f6
Vinícius Dias <vyvy292004@gmail.com>
2025-09-19 11:12:45 -0300

    chore: adds unplugin-auto-import

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,18 +3,32 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
+import AutoImport from 'unplugin-auto-import'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss()
+    tailwindcss(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+
+        /\.vue$/, // .vue
+
+        /\.vue\?vue/, // .vue?vue
+
+        /\.md$/, // .md
+      ],
+
+      imports: ['vue'],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
