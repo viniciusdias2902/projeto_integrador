34ac53820d0ea26838647d861654206c6771f0f1
Vinícius Dias <vyvy292004@gmail.com>
2025-09-20 07:44:26 -0300

    chore: adds axios

--- app/src/components/LoginComponent.vue
+++ app/src/components/LoginComponent.vue
@@ -1,3 +1,21 @@
+<script>
+import { login } from '../services/auth'
+
+const email = ref('')
+const password = ref('')
+const error = ref(null)
+
+async function handleLogin(e) {
+  e.preventDefault()
+  try {
+    await login(email.value, password.value)
+    alert('Login feito com sucesso!')
+  } catch (err) {
+    error.value = 'Email ou senha inválidos'
+  }
+}
+</script>
+
 <template>
   <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
     <legend class="fieldset-legend">Login</legend>
@@ -8,7 +26,7 @@
     <label class="label">Senha</label>
     <input type="password" class="input" placeholder="Senha" />
 
-    <button class="btn btn-neutral mt-4">Login</button>
+    <button class="btn btn-neutral mt-4" @click="handleLogin()">Login</button>
     <button class="btn btn-outline mt-4">Cadastro</button>
   </fieldset>
 </template>
