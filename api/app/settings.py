9463c90dd916c0622428c9b0ca12d3e2824f9f5f
Vinícius Dias <vyvy292004@gmail.com>
2025-09-20 08:00:29 -0300

    fix: enable CORS for Vue frontend

--- api/app/settings.py
+++ api/app/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     "students",
     "drivers",
     "polls",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +54,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite
+    "http://127.0.0.1:5173", # se você usar esse também
 ]
 
 ROOT_URLCONF = "app.urls"
