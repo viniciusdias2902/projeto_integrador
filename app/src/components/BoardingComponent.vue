b13539158870c80a57045cdaf2f4da4980bb69b6
Vinícius Dias <vyvy292004@gmail.com>
2025-09-20 06:22:39 -0300

    feat: improve boarding page style

--- app/src/components/BoardingComponent.vue
+++ app/src/components/BoardingComponent.vue
@@ -7,22 +7,32 @@ const props = defineProps({
 <template>
   <div class="shadow-md rounded-box p-6 w-80 flex-col items-center justify-center">
     <h2 class="text-xl font-bold mb-4">{{ boardingType }}</h2>
-    <ol class="list-decimal list-inside space-y-1">
-      <li>Fulano</li>
-      <li>Ciclano</li>
-      <li>Beltrano</li>
-      <li>Fulano</li>
-      <li>Ciclano</li>
-      <li>Beltrano</li>
-      <li>Fulano</li>
-      <li>Ciclano</li>
-      <li>Beltrano</li>
-      <li>Fulano</li>
-      <li>Ciclano</li>
-      <li>Beltrano</li>
-      <li>Fulano</li>
-      <li>Ciclano</li>
-      <li>Beltrano</li>
-    </ol>
+    <div>
+      <ul class="list bg-base-100 rounded-box shadow-md">
+        <li class="list-row">
+          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
+          <div class="list-col-grow">
+            <div>Literalmente a Sabrina Carpenter</div>
+            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
+          </div>
+        </li>
+
+        <li class="list-row">
+          <div class="text-4xl font-thin opacity-30 tabular-nums">02</div>
+          <div class="list-col-grow">
+            <div>Katie Bouman</div>
+            <div class="text-xs uppercase font-semibold opacity-60">MIT</div>
+          </div>
+        </li>
+
+        <li class="list-row">
+          <div class="text-4xl font-thin opacity-30 tabular-nums">03</div>
+          <div class="list-col-grow">
+            <div>Danilo</div>
+            <div class="text-xs uppercase font-semibold opacity-60">Gado Bravo</div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
