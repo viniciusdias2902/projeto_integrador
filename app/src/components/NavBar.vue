2c70ef03505613136b112cf74fdd19ca751b7672
Vinícius Dias <vyvy292004@gmail.com>
2025-09-20 15:17:24 -0300

    feat: custom navbar for routes

--- app/src/components/NavBar.vue
+++ app/src/components/NavBar.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const authRoutes = ['/']
+const appRoutes = ['/enquetes', '/lista-embarque']
+</script>
+
 <template>
   <div class="navbar bg-base-100 shadow-sm">
     <div class="flex-1">
@@ -5,8 +13,25 @@
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1">
-        <li><a class="btn mr-2">Login</a></li>
-        <li><a class="btn mr-2">Cadastro</a></li>
+        <!-- Grupo de autenticação -->
+        <template v-if="authRoutes.includes(route.path)">
+          <li>
+            <RouterLink to="/" class="btn mr-2">Login</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/cadastro" class="btn mr-2">Cadastro</RouterLink>
+          </li>
+        </template>
+
+        <!-- Grupo de app -->
+        <template v-else-if="appRoutes.includes(route.path)">
+          <li>
+            <RouterLink to="/enquetes" class="btn mr-2">Enquetes</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/lista-embarque" class="btn mr-2">Lista de embarque</RouterLink>
+          </li>
+        </template>
       </ul>
     </div>
   </div>
