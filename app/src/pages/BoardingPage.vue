9475e607ef041aad3b70e78f5aab75e3e11a04e8
Vinícius Dias <vyvy292004@gmail.com>
2025-09-20 06:49:43 -0300

    feat: adds responsiveness to the boarding page

--- app/src/pages/BoardingPage.vue
+++ app/src/pages/BoardingPage.vue
@@ -4,7 +4,7 @@ import DefaultLayout from '@/templates/DefaultLayout.vue'
 
 <template>
   <DefaultLayout>
-    <div class="grid grid-cols-2 gap-4">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
       <BoardingComponent boardingType="Ida" />
       <BoardingComponent boardingType="Volta" />
     </div>
