1b3c4edca2f8011abf52e3e869569d73084584a0
Vinícius Dias <vyvy292004@gmail.com>
2025-09-23 11:14:22 -0300

    refactor: adds InputField

---
+++ app/src/components/InputField.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="form-control">
+    <label :for="id" class="label">
+      <span class="label-text">{{ label }}</span>
+    </label>
+    <input
+      :id="id"
+      :type="type"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      required
+      class="input input-bordered w-full"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputField',
+  props: {
+    id: String,
+    label: String,
+    type: {
+      type: String,
+      default: 'text',
+    },
+    modelValue: [String, Number],
+  },
+}
+</script>
