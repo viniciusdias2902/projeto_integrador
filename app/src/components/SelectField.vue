e0985c2deadf42fe5de2c666dc60cc4eb8796cc2
Vinícius Dias <vyvy292004@gmail.com>
2025-09-23 11:14:01 -0300

    refactor: adds SelectField

---
+++ app/src/components/SelectField.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form-control">
+    <label :for="id" class="label">
+      <span class="label-text">{{ label }}</span>
+    </label>
+    <select
+      :id="id"
+      :value="modelValue"
+      @change="$emit('update:modelValue', $event.target.value)"
+      required
+      class="select select-bordered w-full"
+    >
+      <option disabled value="">Selecione</option>
+      <option v-for="option in options" :key="option.value" :value="option.value">
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectField',
+  props: {
+    id: String,
+    label: String,
+    options: Array,
+    modelValue: String,
+  },
+}
+</script>
