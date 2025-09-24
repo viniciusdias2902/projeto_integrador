1e2328fae83925462d375ac2bcddbe465f3f05bb
Vinícius Dias <vyvy292004@gmail.com>
2025-09-24 07:19:30 -0300

    refactor: finishes refactoring boarding component

--- app/src/components/BoardingComponent.vue
+++ app/src/components/BoardingComponent.vue
@@ -1,4 +1,6 @@
 <script setup>
+import ListRow from './ListRow.vue'
+
 const props = defineProps({
   boardingType: String,
 })
@@ -9,13 +11,13 @@ const props = defineProps({
     <h2 class="text-xl font-bold mb-4">{{ boardingType }}</h2>
     <div>
       <ul class="list bg-base-100 rounded-box shadow-md max-h-100 overflow-scroll">
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
+        <ListRow
+          v-for="n in 20"
+          :key="n"
+          :index="n"
+          title="Danilo"
+          description="Gado bravo"
+        ></ListRow>
       </ul>
     </div>
   </div>
--- app/src/components/ListRow.vue
+++ app/src/components/ListRow.vue
@@ -7,7 +7,7 @@ defineProps({ index: Number, title: String, description: String })
     <div class="text-4xl font-thin opacity-30 tabular-nums">{{ index }}</div>
     <div class="list-col-grow">
       <div>{{ title }}</div>
-      <div class="text-xs uppercase font-semibold opacity-60">{{ string }}</div>
+      <div class="text-xs uppercase font-semibold opacity-60">{{ description }}</div>
     </div>
   </li>
 </template>
