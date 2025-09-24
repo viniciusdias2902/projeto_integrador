50b32cb45de20794a61cb67d1d786e1afde8dcb1
Vinícius Dias <vyvy292004@gmail.com>
2025-09-24 06:33:10 -0300

    refactor: adds Alert Component

--- app/src/components/LoginComponent.vue
+++ app/src/components/LoginComponent.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { useRouter } from 'vue-router'
 import { login } from '../services/auth'
+import AlertComponent from './AlertComponent.vue'
 const router = useRouter()
 const email = ref('')
 const password = ref('')
@@ -28,22 +29,7 @@ async function handleLogin() {
 
     <label class="label">Senha</label>
     <input type="password" class="input" placeholder="Senha" v-model="password" />
-    <div role="alert" class="alert alert-error mt-4" v-if="error">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6 shrink-0 stroke-current"
-        fill="none"
-        viewBox="0 0 24 24"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-      <span>{{ error }}</span>
-    </div>
+    <AlertComponent v-if="error">{{ error }}</AlertComponent>
     <button class="btn btn-neutral mt-4" type="submit" @click="handleLogin">Login</button>
     <button class="btn btn-outline mt-4" @click="$router.push('/cadastro')">Cadastro</button>
   </fieldset>
