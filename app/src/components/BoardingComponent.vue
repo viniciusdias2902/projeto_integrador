db037b787765b4bd26f2ed4e68b8dd2260c1897c
Vinícius Dias <vyvy292004@gmail.com>
2025-09-24 07:15:59 -0300

    feat: adds ListRow component

--- app/src/components/BoardingComponent.vue
+++ app/src/components/BoardingComponent.vue
@@ -16,64 +16,6 @@ const props = defineProps({
             <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
           </div>
         </li>
-
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">02</div>
-          <div class="list-col-grow">
-            <div>Katie Bouman</div>
-            <div class="text-xs uppercase font-semibold opacity-60">MIT</div>
-          </div>
-        </li>
-
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">03</div>
-          <div class="list-col-grow">
-            <div>Danilo</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Gado Bravo</div>
-          </div>
-        </li>
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
-        <li class="list-row">
-          <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
-          <div class="list-col-grow">
-            <div>Literalmente a Sabrina Carpenter</div>
-            <div class="text-xs uppercase font-semibold opacity-60">Vale Lehigh</div>
-          </div>
-        </li>
       </ul>
     </div>
   </div>
---
+++ app/src/components/ListRow.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({ index: Number, title: String, description: String })
+</script>
+
+<template>
+  <li class="list-row">
+    <div class="text-4xl font-thin opacity-30 tabular-nums">{{ index }}</div>
+    <div class="list-col-grow">
+      <div>{{ title }}</div>
+      <div class="text-xs uppercase font-semibold opacity-60">{{ string }}</div>
+    </div>
+  </li>
+</template>
