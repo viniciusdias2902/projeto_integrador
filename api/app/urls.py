bf6a7a3e018710bd2f3afde05f7563241d4ce76b
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-25 03:14:21 -0300

    feat: adds DefaultRouter in boarding list url

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from common.views import BoardingPointViewSet
+
+router = DefaultRouter()
+
+router.register(r'boarding-points', BoardingPointViewSet, basename='boarding-point')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,4 +13,5 @@ urlpatterns = [
     path("api/v1/", include("drivers.urls")),
     path("api/v1/", include("polls.urls")),
     
+    path("api/v1/", include(router.urls)),
 ]
