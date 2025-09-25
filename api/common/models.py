0c46810c279c1b4637485e57a0925400474fc468
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-25 02:52:23 -0300

    feat: adds address reference attribute

--- api/common/models.py
+++ api/common/models.py
@@ -12,6 +12,7 @@ class Person(models.Model):
 
 class BoardingPoint(models.Model):
     name = models.CharField(max_length=255, help_text="Ex: Praça Central, Posto Shell")
+    address_reference = models.CharField(max_length=255, blank=True, null=True, help_text="Ex: Em frente à farmácia")
     route_order = models.PositiveIntegerField(default=0, help_text="Ordem em que o motorista passa no ponto (0, 1, 2...)")
 
     class Meta:
