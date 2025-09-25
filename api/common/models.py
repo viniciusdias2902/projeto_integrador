e25eeae16f7dfc6e8162e89a4eee2a273d38ae4d
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-25 02:51:05 -0300

    feat: adds boarding_point model

--- api/common/models.py
+++ api/common/models.py
@@ -9,3 +9,13 @@ class Person(models.Model):
 
     class Meta:
         abstract = True
+
+class BoardingPoint(models.Model):
+    name = models.CharField(max_length=255, help_text="Ex: Praça Central, Posto Shell")
+    route_order = models.PositiveIntegerField(default=0, help_text="Ordem em que o motorista passa no ponto (0, 1, 2...)")
+
+    class Meta:
+        ordering = ['route_order'] # Ordena os pontos por padrão
+
+    def __str__(self):
+        return f"{self.route_order}: {self.name}"
