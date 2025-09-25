0fee25453a4c794ab3669f8f07b65f73456b8ad8
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-25 02:58:31 -0300

    feat: adds boarding_point attribute in students model

--- api/students/models.py
+++ api/students/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from common.constants import SHIFT_CHOICES
 from common.models import Person
+from common.models import BoardingPoint
 
 UNIVERSITY_CHOICES = (
     ("UESPI", "Universidade Estadual do Piauí"),
@@ -15,3 +16,10 @@ class Student(Person):
     registration_date = models.DateField(auto_now_add=True)
     class_shift = models.CharField(choices=SHIFT_CHOICES, blank=False, null=False)
     university = models.CharField(choices=UNIVERSITY_CHOICES)
+
+    boarding_point = models.ForeignKey(
+        BoardingPoint, 
+        null=True,
+        blank=True,
+        related_name="students"
+    )
