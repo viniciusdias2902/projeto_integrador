3c2c65f8cff503e87fc90792a3a7149e65ac5c33
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-25 19:19:52 -0300

    refactor: changed BoardingCompononent for interact polls service

--- app/src/components/BoardingComponent.vue
+++ app/src/components/BoardingComponent.vue
@@ -1,24 +1,48 @@
 <script setup>
-import ListRow from './ListRow.vue'
+import { ref, onMounted } from 'vue';
+import { getBoardingList } from '@/services/polls';
+import ListRow from './ListRow.vue';
 
-defineProps({
+const props = defineProps({
   boardingType: String,
-})
+});
+
+const boardingPoints = ref([]); 
+const isLoading = ref(true);    
+const error = ref(null); 
+
+const loadBoardingData = async () => {
+  try {
+    const pollId = 1;
+    
+    const tripType = props.boardingType === 'Ida' ? 'one_way_outbound' : 'one_way_return';
+
+    const data = await getBoardingList(pollId, tripType);
+    boardingPoints.value = data; 
+  } catch (err) {
+    error.value = err.message; 
+  } finally {
+    isLoading.value = false; 
+  }
+};
+
+
+onMounted(() => {
+  loadBoardingData();
+});
 </script>
 
 <template>
-  <div class="shadow-md rounded-box p-6 w-80 flex-col items-center justify-center">
+  <div class="shadow-md rounded-box p-6 w-full flex-col">
     <h2 class="text-xl font-bold mb-4">{{ boardingType }}</h2>
-    <div>
-      <ul class="list bg-base-100 rounded-box shadow-md max-h-100 overflow-scroll">
-        <ListRow
-          v-for="n in 20"
-          :key="n"
-          :index="n"
-          title="Danilo"
-          description="Gado bravo"
-        ></ListRow>
-      </ul>
+
+    
+    <div v-else-if="error">
+      <p class="text-red-500">Erro: {{ error }}</p>
+    </div>
+    
+    <div v-else-if="boardingPoints.length === 0">
+      <p>Nenhum aluno confirmado para esta viagem.</p>
     </div>
   </div>
 </template>
