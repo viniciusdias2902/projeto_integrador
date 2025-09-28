4ef839c8d423a5707b8f0693fda4dd3aa1659c3c
gabruurr <gabryelaraujo2019@gmail.com>
2025-09-28 17:49:24 -0300

    fix: fixes import in urls.py main file

--- api/app/urls.py
+++ api/app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from common.views import BoardingPointViewSet
+from boarding_points.views import BoardingPointViewSet
 
 router = DefaultRouter()
 
