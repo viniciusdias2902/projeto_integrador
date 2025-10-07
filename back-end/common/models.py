4252c8e735afab8595097dc3801252ca365ce1fa
Vinícius Dias <vyvy292004@gmail.com>
2025-10-07 10:49:57 -0300

    feat: adds role_choices

--- back-end/common/models.py
+++ back-end/common/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+ROLE_CHOICES = (
+    ("student", "Student"),
+    ("employee", "Employee"),
+    ("admin", "Administrator"),
+)
+
+
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
