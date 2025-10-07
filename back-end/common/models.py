a974a219b9e25579fe8dfbc45777af975dbb3547
Vinícius Dias <vyvy292004@gmail.com>
2025-10-07 10:50:19 -0300

    feat: adds role

--- back-end/common/models.py
+++ back-end/common/models.py
@@ -13,6 +13,7 @@ class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=11)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     class Meta:
         abstract = True
