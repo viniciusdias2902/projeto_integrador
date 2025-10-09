7a0291c7d2af4d0af9912a490c495b6f1b4bab6b
gabruurr <gabryelaraujo2019@gmail.com>
2025-10-09 01:05:38 -0300

    chore: fixes folder structure

--- front-end/node_modules/unplugin-auto-import/dist/esbuild.d.ts
+++
@@ -1,7 +0,0 @@
-import { Options } from "./types-CxCu6kF3.js";
-import * as esbuild0 from "esbuild";
-
-//#region src/esbuild.d.ts
-declare const _default: (options: Options) => esbuild0.Plugin;
-//#endregion
-export { _default as default };
