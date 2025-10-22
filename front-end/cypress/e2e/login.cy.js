db521a7a9f334c865dbb5c61c38723b4371f243b
gabruurr <gabryelaraujo2019@gmail.com>
2025-10-22 03:53:31 -0300

    test: tests invalid credentials in login page

--- front-end/cypress/e2e/login.cy.js
+++ front-end/cypress/e2e/login.cy.js
@@ -16,4 +16,23 @@ describe('Funcionalidade de Login', () => {
     cy.window().its('localStorage').invoke('getItem', 'access').should('exist');
   });
 
+  it('Deve mostrar mensagem de erro com credenciais inválidas', () => {
+    const invalidEmail = 'usuario@errado.com';
+    const invalidPassword = 'senhaerrada';
+
+    cy.get('input[type="email"]').type(invalidEmail);
+    cy.get('input[type="password"]').type(invalidPassword);
+
+    cy.contains('button', 'Login').click();
+
+    cy.get('.alert-error', { timeout: 10000 })
+      .should('be.visible')
+      .and('contain.text', 'Email ou senha inválidos');
+    cy.url().should('not.include', '/enquetes');
+  });
+
+  it('Deve mostrar erro se tentar logar com campos vazios', () => {
+     cy.contains('button', 'Login').click();
+     cy.url().should('not.include', '/enquetes');
+  });
 });
