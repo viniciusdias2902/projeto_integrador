9c441e4d5cb0fb5cb05a2dc42b257b4a5f665ca0
Vinícius Dias <vyvy292004@gmail.com>
2025-10-22 03:41:02 -0300

    feat: adds a simple trips page

---
+++ front-end/src/pages/TripsPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultLayout from '@/templates/DefaultLayout.vue'
+</script>
+
+<template>
+  <DefaultLayout>
+    <div class="container mx-auto px-4 py-8">
+      <h1 class="text-4xl font-bold text-center">Viagens</h1>
+      <p class="text-center text-base-content/70 mt-4">
+        Página de gerenciamento de viagens (em desenvolvimento)
+      </p>
+    </div>
+  </DefaultLayout>
+</template>
