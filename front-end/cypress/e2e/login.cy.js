9d527ee91569d71dc8f748cc95ccc40d0c5cc8ba
gabruurr <gabryelaraujo2019@gmail.com>
2025-10-22 03:51:59 -0300

    test: tests valid user login in login page

---
+++ front-end/cypress/e2e/login.cy.js
@@ -0,0 +1,19 @@
+describe('Funcionalidade de Login', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('Deve fazer login com sucesso com credenciais válidas e redirecionar', () => {
+    const userEmail = 'ana.silva@example.com';
+    const userPassword = 'Password123';
+
+    cy.get('input[type="email"]').type(userEmail);
+    cy.get('input[type="password"]').type(userPassword);
+
+    cy.contains('button', 'Login').click();
+
+    cy.url().should('include', '/enquetes');
+    cy.window().its('localStorage').invoke('getItem', 'refresh').should('exist');
+  });
+
+});
