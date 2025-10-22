93d058b51e496d0b55b97f82b8529f8b213d974f
gabruurr <gabryelaraujo2019@gmail.com>
2025-10-22 03:58:23 -0300

    test: tests if student can vote

---
+++ front-end/cypress/e2e/voting.cy.js
@@ -0,0 +1,25 @@
+describe('Funcionalidade de Votação nas Enquetes', () => {
+  beforeEach(() => {
+    cy.request({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v1/authentication/token/',
+      body: { username: 'ana.silva@example.com', password: 'Password123' },
+    }).then((response) => {
+      window.localStorage.setItem('access', response.body.access);
+      window.localStorage.setItem('refresh', response.body.refresh);
+    });
+    cy.visit('/enquetes');
+    cy.wait(1500);
+  });
+
+  it('Deve permitir que um aluno vote em uma opção e mostrar sucesso', () => {
+    cy.contains('fieldset', 'Sexta', { timeout: 10000 }).should('be.visible').within(() => {
+      cy.contains('label', 'Ida e volta').find('input[type="radio"]').check();
+      cy.contains('button', 'Enviar Voto').click();
+
+      cy.get('.alert-success', { timeout: 10000 })
+        .should('be.visible')
+        .and('contain.text', 'Voto enviado com sucesso!');
+    });
+  });
+});
