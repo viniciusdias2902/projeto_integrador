801c7f5374ae0a7f578c054232300fb3399e0d1e
Vinícius Dias <vyvy292004@gmail.com>
2025-11-04 12:19:52 -0300

    fix: adds admin model to the admins

--- back-end/admins/models.py
+++ back-end/admins/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
+from common.models import Person
 
-# Create your models here.
+
+class Admin(Person):
+    def save(self, *args, **kwargs):
+        if not self.role:
+            self.role = "admin"
+
+        super().save(*args, **kwargs)
+
+        if self.user:
+            group, _ = Group.objects.get_or_create(name="admins")
+            self.user.groups.add(group)
+
+            if not self.user.is_superuser:
+                self.user.is_superuser = True
+                self.user.is_staff = True
+                self.user.save()
+
+    def __str__(self):
+        return f"Admin: {self.name}"
