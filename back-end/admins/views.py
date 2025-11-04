11b23af879d8bf25ffcbe7a78c469f07875a06bf
Vinícius Dias <vyvy292004@gmail.com>
2025-11-04 12:20:47 -0300

    fix: adds admin views

--- back-end/admins/views.py
+++ back-end/admins/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Admin
+from .serializers import AdminSerializer, AdminCreateSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from common.permissions import GlobalDefaultPermission
 
-# Create your views here.
+
+class AdminListCreateView(ListCreateAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser)
+    queryset = Admin.objects.all()
+
+    def get_serializer_class(self):
+        if self.request.method == "POST":
+            return AdminCreateSerializer
+        return AdminSerializer
+
+
+class AdminRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated, IsAdminUser)
+    queryset = Admin.objects.all()
+    serializer_class = AdminSerializer
