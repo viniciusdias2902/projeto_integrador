96d26c0c7aaf351ebd8d3f7a13e96ab5c58dec8c
Vinícius Dias <vyvy292004@gmail.com>
2025-11-04 12:07:30 -0300

    feat: update students url

--- back-end/students/urls.py
+++ back-end/students/urls.py
@@ -10,4 +10,19 @@ urlpatterns = [
         views.StudentRetrieveUpdateDestroyView.as_view(),
         name="students-retrieve-update-destroy",
     ),
+    path(
+        "students/<int:pk>/payment/",
+        views.StudentPaymentUpdateView.as_view(),
+        name="student-payment-update",
+    ),
+    path(
+        "students/payments/",
+        views.StudentPaymentListView.as_view(),
+        name="student-payment-list",
+    ),
+    path(
+        "students/payments/bulk-update/",
+        views.StudentPaymentBulkUpdateView.as_view(),
+        name="student-payment-bulk-update",
+    ),
 ]
