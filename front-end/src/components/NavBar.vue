a5cd75d48ec7fe029eb7a8cd217110585d7ab376
Vinícius Dias <vyvy292004@gmail.com>
2025-11-04 12:53:11 -0300

    feat: updates navbar

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -4,11 +4,14 @@ import { logout } from '@/services/auth'
 import { useAuth } from '@/useAuth'
 
 const route = useRoute()
-const { isStudent, isDriver, isAuthenticated } = useAuth()
+const { isStudent, isDriver, isAuthenticated, userRole } = useAuth()
 
 const authRoutes = ['/', '/cadastro']
 const studentRoutes = ['/enquetes', '/lista-embarque', '/acompanhar-viagem']
 const driverRoutes = ['/viagens', '/lista-embarque']
+const adminRoutes = ['/admin/estudantes']
+
+const isAdmin = computed(() => userRole.value === 'admin')
 </script>
 
 <template>
@@ -48,6 +51,31 @@ const driverRoutes = ['/viagens', '/lista-embarque']
           </li>
           <li>
             <RouterLink to="/lista-embarque" class="btn mr-2">Lista de embarque</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/" class="btn mr-2" @click="logout">Sair</RouterLink>
+          </li>
+        </template>
+
+        <template v-else-if="isAdmin && isAuthenticated">
+          <li>
+            <RouterLink to="/admin/estudantes" class="btn mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4 mr-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              </svg>
+              Estudantes
+            </RouterLink>
           </li>
           <li>
             <RouterLink to="/" class="btn mr-2" @click="logout">Sair</RouterLink>
