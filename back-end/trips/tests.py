7c9172ed5f2b2ea24c115ca4b240c4953b6a686d
gabruurr <gabryelaraujo2019@gmail.com>
2025-11-07 01:18:12 -0300

    fix: fixes assertion lenght HTTP message response

--- back-end/trips/tests.py
+++ back-end/trips/tests.py
@@ -94,7 +94,7 @@ class TripAPITestCase(APITestCase):
         url = reverse("trip-list")
         response = self.client.get(url, {"status": "pending"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data), 2)
         response = self.client.get(url, {"trip_type": "outbound"})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
