291c8219aa27137009f0256f941bf87e31e7069c
Vinícius Dias <vyvy292004@gmail.com>
2025-11-07 09:45:36 -0300

    feat: change the navbar  to add the dashboard

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -9,7 +9,7 @@ const { isStudent, isDriver, isAuthenticated, userRole } = useAuth()
 const authRoutes = ['/', '/cadastro']
 const studentRoutes = ['/enquetes', '/lista-embarque', '/acompanhar-viagem']
 const driverRoutes = ['/viagens', '/lista-embarque']
-const adminRoutes = ['/admin/estudantes']
+const adminRoutes = ['/admin/estudantes', '/admin/dashboard']
 
 const isAdmin = computed(() => {
   const role = (userRole.value || '').toLowerCase()
@@ -61,6 +61,25 @@ const isAdmin = computed(() => {
         </template>
 
         <template v-else-if="isAdmin && isAuthenticated">
+          <li>
+            <RouterLink to="/admin/dashboard" class="btn mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4 mr-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+              Dashboard
+            </RouterLink>
+          </li>
           <li>
             <RouterLink to="/admin/estudantes" class="btn mr-2">
               <svg
