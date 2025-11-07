e228752dde6a0614735b151050427ef89cd21bea
gabruurr <gabryelaraujo2019@gmail.com>
2025-11-07 01:07:26 -0300

    fix: corrects boarding type

--- back-end/trips/tests.py
+++ back-end/trips/tests.py
@@ -1,14 +1,15 @@
+from django.urls import reverse
 from rest_framework.test import APITestCase
+from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.utils import timezone
 from datetime import date
-
 
 from students.models import Student
 from boarding_points.models import BoardingPoint
 from polls.models import Poll, Vote
 from .models import Trip
-
 
 class TripAPITestCase(APITestCase):
 
@@ -35,41 +36,23 @@ class TripAPITestCase(APITestCase):
         )
 
         self.student_ana = Student.objects.create(
-            user=self.student_user_1,
-            name="Ana Silva",
-            phone="111",
-            class_shift="M",
-            university="UESPI",
-            boarding_point=self.ponto_a,
+            user=self.student_user_1, name="Ana Silva", phone="111",
+            class_shift="M", university="UESPI", boarding_point=self.ponto_a
         )
         self.student_bruno = Student.objects.create(
-            user=self.student_user_2,
-            name="Bruno Costa",
-            phone="222",
-            class_shift="A",
-            university="IFPI",
-            boarding_point=self.ponto_a,
+            user=self.student_user_2, name="Bruno Costa", phone="222",
+            class_shift="A", university="IFPI", boarding_point=self.ponto_a
         )
         self.student_carla = Student.objects.create(
-            user=self.student_user_3,
-            name="Carla Dias",
-            phone="333",
-            class_shift="N",
-            university="IFPI",
-            boarding_point=self.ponto_b,
+            user=self.student_user_3, name="Carla Dias", phone="333",
+            class_shift="N", university="IFPI", boarding_point=self.ponto_b
         )
-
+        
         self.poll = Poll.objects.create(date=date.today())
 
-        Vote.objects.create(
-            student=self.student_ana, poll=self.poll, option="round_trip"
-        )
-        Vote.objects.create(
-            student=self.student_bruno, poll=self.poll, option="one_way_outbound"
-        )
-        Vote.objects.create(
-            student=self.student_carla, poll=self.poll, option="one_way_return"
-        )
+        Vote.objects.create(student=self.student_ana, poll=self.poll, option="round_trip")
+        Vote.objects.create(student=self.student_bruno, poll=self.poll, option="one_way_outbound")
+        Vote.objects.create(student=self.student_carla, poll=self.poll, option="round_trip") # <-- NOVO VOTO
 
         self.trip_outbound = Trip.objects.create(poll=self.poll, trip_type="outbound")
         self.trip_return = Trip.objects.create(poll=self.poll, trip_type="return")
@@ -81,7 +64,7 @@ class TripAPITestCase(APITestCase):
     def authenticate_admin(self):
         token = self.get_jwt_token(self.admin_user)
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
-
+        
     def authenticate_student(self, user=None):
         user = user or self.student_user_1
         token = self.get_jwt_token(user)
