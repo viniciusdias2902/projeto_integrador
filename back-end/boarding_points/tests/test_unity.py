16b8a552e045675dabbd93c656d07f70c2ae76a3
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-10 20:24:14 -0300

    test(boarding_points): ensure BoardingPoints are ordered by route_order field

--- back-end/boarding_points/tests/test_unity.py
+++ back-end/boarding_points/tests/test_unity.py
@@ -10,3 +10,15 @@ class BoardingPointTests(TestCase):
         self.assertEqual(boarding_point.name, "Hotel Alvorada")
         self.assertEqual(boarding_point.address_reference, "Em frente ao IBE")
         self.assertEqual(boarding_point.route_order, 1)
+
+    def test_boarding_points_ordering(self):
+        boarding_points1 = BoardingPoint.objects.create(
+            name="Rodoviária", route_order=2
+        )
+        boarding_points2 = BoardingPoint.objects.create(
+            name="Machado AutoPeças", route_order=1
+        )
+
+        boarding_points = list(BoardingPoint.objects.all())
+        self.assertEqual(boarding_points[0], boarding_points2)
+        self.assertEqual(boarding_points[1], boarding_points1)
