4e16067a58e134486a4f1b35ecc28aa943f6da90
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-11 20:09:10 -0300

    test(drivers): add test to ensure Driver role is set on save

--- back-end/drivers/tests/test_unity.py
+++ back-end/drivers/tests/test_unity.py
@@ -2,4 +2,17 @@ from drivers.models import Driver
 from django.test import TestCase
 from django.contrib.auth.models import User, Group
 
+
 class DriverModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="dom", password="password123")
+
+    def test_driver_role_is_set_to_driver_on_save(self):
+        driver = Driver.objects.create(
+            user=self.user,
+            name="Sem usuário",
+            phone="1234567",
+            shift="A",
+            dailyPaymentCents=10000,
+        )
+        self.assertEqual(driver.role, "driver")
