a170a7d1d2c2d5f365a36c3d1381a66cada4bb0f
Vinícius Dias <vyvy292004@gmail.com>
2025-11-11 13:26:51 -0300

    feat: adds boarding point admin to the navba

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -81,11 +81,30 @@ const isAdmin = computed(() => {
             </RouterLink>
           </li>
           <li>
-            <RouterLink to="/" class="btn mr-2" @click="logout">Sair</RouterLink>
+            <RouterLink to="/admin/pontos-embarque" class="btn mr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-4 w-4 mr-1"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+              </svg>
+              Pontos
+            </RouterLink>
           </li>
-        </template>
-
-        <template v-else-if="isAuthenticated">
           <li>
             <RouterLink to="/" class="btn mr-2" @click="logout">Sair</RouterLink>
           </li>
