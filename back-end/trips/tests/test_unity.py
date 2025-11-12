0dde6fa840018973d730ef539a0375ca87a0b9a9
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-12 18:44:57 -0300

    test(trips): start tests for views and serializers

---
+++ back-end/trips/tests/test_unity.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from trips.models import Trips
+from django.contrib.auth.models import User
+from trips.views import TripCreateView, TripCompleteView
+from trips.serializers import TripSerializer, TripDetailSerializer 
+
+
+class TripsTest(TestCase):
+    def test
