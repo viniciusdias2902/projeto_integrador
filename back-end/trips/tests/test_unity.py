34d07df84299e82edb326f0af13f547e04820df8
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-12 19:16:47 -0300

    test(trip): add ct01 to verify default status pending when creating a Trip

--- back-end/trips/tests/test_unity.py
+++ back-end/trips/tests/test_unity.py
@@ -1,9 +1,14 @@
-# from django.test import TestCase
-# from trips.models import Trips
-# from django.contrib.auth.models import User
-# from trips.views import TripCreateView, TripCompleteView
-# from trips.serializers import TripSerializer, TripDetailSerializer
+from django.test import TestCase
+from trips.models import Trip
+from django.contrib.auth.models import User
+from trips.views import TripCreateView, TripCompleteView
+from trips.serializers import TripSerializer, TripDetailSerializer
+from polls.models import Poll
+from datetime import date
 
 
-# class TripsTest(TestCase):
-#   def test
+class TripsTest(TestCase):
+    def test_ct01_default_status_is_pending(self):
+        poll = Poll.objects.create(date=date(2025, 9, 13), status="open")
+        trip = Trip.objects.create(poll=poll, trip_type="outbound")
+        self.assertEqual(trip.status, "pending")
