1867a901a8b80b3d6df94cd67a750234ddba7251
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-12 19:01:14 -0300

    test(poll): add ct01 to validate __str__ method of Poll model

--- back-end/polls/tests/test_unity.py
+++ back-end/polls/tests/test_unity.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
-from polls.models import Polls
+from polls.models import Poll
 from django.contrib.auth.models import User
 from polls.views import PollListView, PollDetailView, PollBoardingListView
 from polls.serializers import PollSerializer
+from datetime import date
 
 
 class PollsTest(TestCase):
-    def test
+    def test_ct01_str_method(self):
+        poll = Poll.objects.create(date=date(2025, 8, 14), status="open")
+        self.assertEqual(str(poll), "Poll for 2025-08-14 (open)")
