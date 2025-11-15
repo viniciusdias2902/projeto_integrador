8232b90ccbece007c8605a31cb55d0a961a3572b
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-15 20:35:02 -0300

    test: update ct02 to use TestCase attributes and validate correct Vote creation (persistence and option value)

--- back-end/polls/tests/test_unity.py
+++ back-end/polls/tests/test_unity.py
@@ -1,19 +1,25 @@
 from django.test import TestCase
-from polls.models import Poll, Vote
+from polls.models import Poll, Vote, Student
 from django.contrib.auth.models import User
 from polls.views import PollListView, PollDetailView, PollBoardingListView
 from polls.serializers import PollSerializer
 from datetime import date
+from django.utils import timezone
+
 
 class PollsTest(TestCase):
     def setUp(self):
+        self.user = User.objects.create(username="teste_user")
+        self.student = Student.objects.create(name="Teste enquetes", user=self.user)
+        self.poll_today = Poll.objects.create(date=timezone.localdate())
+
     def test_ct01_str_method(self):
         poll = Poll.objects.create(date=date(2025, 8, 14), status="open")
         self.assertEqual(str(poll), "Poll for 2025-08-14 (open)")
 
-    def test_ct02_create_valid_vote(self)::
+    def test_ct02_create_valid_vote(self):
         vote = Vote.objects.create(
-            student=student, poll=poll_today, option="round_trip"
+            student=self.student, poll=self.poll_today, option="round_trip"
         )
-        assert vote.pk is not None
-        assert vote.option == "round_trip"
+        self.assertIsNotNone(vote.pk)
+        self.assertEqual(vote.option, "round_trip")
