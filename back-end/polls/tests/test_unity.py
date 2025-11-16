327483337c92d333cebfb6a1b4530a8b8bf3b00e
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-15 21:05:29 -0300

    test: ensure voting is not allowed on yesterday's Poll using mocked time

--- back-end/polls/tests/test_unity.py
+++ back-end/polls/tests/test_unity.py
@@ -3,9 +3,10 @@ from polls.models import Poll, Vote, Student
 from django.contrib.auth.models import User
 from polls.views import PollListView, PollDetailView, PollBoardingListView
 from polls.serializers import PollSerializer
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime, time
 from django.utils import timezone
 from django.db import IntegrityError
+from unittest.mock import patch
 
 
 class PollsTest(TestCase):
@@ -13,8 +14,9 @@ class PollsTest(TestCase):
         self.user = User.objects.create(username="teste_user")
         self.student = Student.objects.create(name="Teste enquetes", user=self.user)
         self.poll_today = Poll.objects.create(date=timezone.localdate())
-        self poll_yesterday = Poll.objects.create(date = timezone.localdate() - timedelta(days=1), status = "open")
-
+        self.poll_yesterday = Poll.objects.create(
+            date=timezone.localdate() - timedelta(days=1), status="open"
+        )
 
     def test_ct01_str_method(self):
         poll = Poll.objects.create(date=date(2025, 8, 14), status="open")
@@ -35,4 +37,13 @@ class PollsTest(TestCase):
         with self.assertRaises(IntegrityError):
             Vote.objects.create(
                 student=self.student, poll=self.poll_today, option="absent"
+            )
+
+    def test_ct04_can_vote_past_poll(self):
+        fake = datetime.combine(timezone.localdate(), time(10, 0))
+        with patch("django.utils.timezone.now") as mock_now:
+            mock_now.return_value = timezone.make_aware(fake)
+
+            self.assertFalse(
+                self.poll_yesterday.can_vote_for_option("one_way_outbound")
             )
