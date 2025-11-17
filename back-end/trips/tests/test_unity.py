621f46b1bbd7829b2c3029f8f6891421f58aa107
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-17 13:34:30 -0300

    Adjust test user creation to avoid unique key conflicts and add test_ct02_str_method to validate the string representation of Trip.

--- back-end/trips/tests/test_unity.py
+++ back-end/trips/tests/test_unity.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from trips.models import Trip, Student, BoardingPoint, Poll, Vote
+from trips.models import Trip
+from students.models import Student
+from polls.models import Poll, Vote
+from boarding_points.models import BoardingPoint
 from django.contrib.auth.models import User
 from trips.views import TripCreateView, TripCompleteView
 from trips.serializers import TripSerializer, TripDetailSerializer
@@ -9,12 +12,13 @@ from datetime import date
 
 class TripsTest(TestCase):
     def setUp(self):
-        self.user = User.objects.create(username="user1")
+        self.user1 = User.objects.create(username="user1")
+        self.user2 = User.objects.create(username="user2")
         self.student1 = Student.objects.create(
-            name="José", user=self.user, university="UESPI"
+            name="José", user=self.user1, university="UESPI"
         )
         self.student2 = Student.objects.create(
-            name="Leonardo", user=self.user, university="CHRISFAPI"
+            name="Leonardo", user=self.user2, university="CHRISFAPI"
         )
         self.boarding1 = BoardingPoint.objects.create(name="Point 1", route_order=1)
         self.boarding2 = BoardingPoint.objects.create(name="Point2", route_order=2)
@@ -36,3 +40,7 @@ class TripsTest(TestCase):
         poll = Poll.objects.create(date=date(2025, 9, 13), status="open")
         trip = Trip.objects.create(poll=poll, trip_type="outbound")
         self.assertEqual(trip.status, "pending")
+
+    def test_ct02_str_method(self):
+        self.assertIn("Trip outbound", str(self.trip_outbound))
+        self.assertIn(str(self.poll.date), str(self.trip_outbound))
