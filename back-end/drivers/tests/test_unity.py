1b43255c3d43a74d2c83c5b283c4a3be3e90c4ca
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-11-18 20:36:11 -0300

    test(driver): add CT01 to driver role test name

--- back-end/drivers/tests/test_unity.py
+++ back-end/drivers/tests/test_unity.py
@@ -7,7 +7,7 @@ class DriverModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="dom", password="password123")
 
-    def test_driver_role_is_set_to_driver_on_save(self):
+    def test_ct01_driver_role_is_set_to_driver_on_save(self):
         driver = Driver.objects.create(
             user=self.user,
             name="Sem usuário",
