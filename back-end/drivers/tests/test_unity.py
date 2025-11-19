e408b8f9826cd52f589eaab7733326b2902a6876
gabruurr <gabryelaraujo2019@gmail.com>
2025-11-19 02:34:57 -0300

    refactor: refactores drivers test cases

--- back-end/drivers/tests/test_unity.py
+++ back-end/drivers/tests/test_unity.py
@@ -1,13 +1,15 @@
-from drivers.models import Driver
 from django.test import TestCase
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
+from drivers.models import Driver
 
 
-class DriverModelTest(TestCase):
+class DriverModelTests(TestCase):
+
     def setUp(self):
         self.user = User.objects.create(username="dom", password="password123")
 
-    def test_ct01_driver_role_is_set_to_driver_on_save(self):
+    # Funcionalidade 1
+    def test_CT_01_driver_role_is_set_to_driver_on_save(self):
         driver = Driver.objects.create(
             user=self.user,
             name="Sem usuário",
@@ -15,4 +17,5 @@ class DriverModelTest(TestCase):
             shift="A",
             dailyPaymentCents=10000,
         )
+
         self.assertEqual(driver.role, "driver")
