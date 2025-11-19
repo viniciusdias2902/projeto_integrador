d01c25cf9f0609c0ba3c3e4711f05079b61f8347
gabruurr <gabryelaraujo2019@gmail.com>
2025-11-19 02:10:26 -0300

    style: improved indentation

--- back-end/admins/tests/test_unity.py
+++ back-end/admins/tests/test_unity.py
@@ -7,21 +7,22 @@ from admins.admin import AdminModelAdmin
 from admins.views import AdminListCreateView, AdminRetrieveUpdateDestroyView
 from admins.serializers import AdminSerializer, AdminCreateSerializer
 
+
 # Funcionalidades 1 e 2
 class AdminModelTests(TestCase):
-    
+
     def test_CT_01_admin_role_automatically_set(self):
         user = User.objects.create_user("testrole", "role@test.com", "pass123")
         admin = Admin.objects.create(user=user, name="Test Role", phone="123")
-        
+
         self.assertEqual(admin.role, "admin")
 
     def test_CT_02_admin_permissions_and_groups_set(self):
         user = User.objects.create_user("testgroup", "group@test.com", "pass123")
         Group.objects.create(name="admins")
-        
+
         Admin.objects.create(user=user, name="Test Group", phone="123")
-        
+
         self.assertTrue(user.groups.filter(name="admins").exists())
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
@@ -29,8 +30,8 @@ class AdminModelTests(TestCase):
     def test_CT_03_str_method(self):
         user = User.objects.create_user("strtest", "str@test.com", "pass123")
         admin = Admin.objects.create(user=user, name="Admin Teste Str", phone="123")
-        
-        self.assertEqual(str(admin), "Admin: Admin Teste Str") 
+
+        self.assertEqual(str(admin), "Admin: Admin Teste Str")
 
 
 # Funcionalidade 4
@@ -43,12 +44,12 @@ class AdminCreateSerializerTests(TestCase):
             "email": "mariarita@teste.com",
             "password": "senha123",
         }
-        #mcock grupos
+        # mcock grupos
         Group.objects.create(name="admins")
 
         serializer = AdminCreateSerializer(data=data)
         self.assertTrue(serializer.is_valid())
-        
+
         admin = serializer.save()
 
         self.assertIsInstance(admin, Admin)
@@ -59,7 +60,7 @@ class AdminCreateSerializerTests(TestCase):
         self.assertEqual(user.username, "mariarita@teste.com")
         self.assertEqual(user.email, "mariarita@teste.com")
         self.assertTrue(user.check_password("senha123"))
-        
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.groups.filter(name="admins").exists())
@@ -73,24 +74,26 @@ class AdminViewTests(TestCase):
 
     def test_CT_04_get_serializer_class_get_method(self):
         view = AdminListCreateView()
-        view.request = self.factory.get('/')
+        view.request = self.factory.get("/")
         self.assertEqual(view.get_serializer_class(), AdminSerializer)
 
     def test_CT_05_get_serializer_class_post_method(self):
         view = AdminListCreateView()
-        view.request = self.factory.post('/')
+        view.request = self.factory.post("/")
         self.assertEqual(view.get_serializer_class(), AdminCreateSerializer)
 
     def test_CT_07_to_09_retrieve_view_configuration(self):
         view = AdminRetrieveUpdateDestroyView()
-        
+
         self.assertEqual(view.queryset.model, Admin)
-        
+
         self.assertEqual(view.serializer_class, AdminSerializer)
-        
-       
+
         perm_classes = view.permission_classes
-        self.assertTrue(any(issubclass(p, IsAuthenticated) for p in perm_classes) or IsAuthenticated in perm_classes)
+        self.assertTrue(
+            any(issubclass(p, IsAuthenticated) for p in perm_classes)
+            or IsAuthenticated in perm_classes
+        )
 
 
 # Funcionalidade 6
--- back-end/boarding_points/tests/test_unity.py
+++ back-end/boarding_points/tests/test_unity.py
@@ -1,40 +1,35 @@
 from django.test import TestCase
 from boarding_points.models import BoardingPoint
 
+
 class BoardingPointModelTests(TestCase):
 
-# funcionaldiade 1
+    # funcionaldiade 1
     def test_CT_01_create_boarding_point_attributes(self):
         boarding_point = BoardingPoint.objects.create(
-            name="Hotel Alvorada", 
-            address_reference="Em frente ao IBE", 
-            route_order=1
+            name="Hotel Alvorada", address_reference="Em frente ao IBE", route_order=1
         )
-        
+
         self.assertEqual(boarding_point.name, "Hotel Alvorada")
         self.assertEqual(boarding_point.address_reference, "Em frente ao IBE")
         self.assertEqual(boarding_point.route_order, 1)
 
-# Funcionalidade 2
+    # Funcionalidade 2
     def test_CT_02_boarding_points_ordering(self):
-       
-        ponto_2 = BoardingPoint.objects.create(
-            name="Rodoviária", route_order=2
-        )
+
+        ponto_2 = BoardingPoint.objects.create(name="Rodoviária", route_order=2)
         # Cria o ponto 1 depois
-        ponto_1 = BoardingPoint.objects.create(
-            name="Machado AutoPeças", route_order=1
-        )
+        ponto_1 = BoardingPoint.objects.create(name="Machado AutoPeças", route_order=1)
 
         pontos_do_banco = list(BoardingPoint.objects.all())
 
         self.assertEqual(pontos_do_banco[0], ponto_1)
         self.assertEqual(pontos_do_banco[0].name, "Machado AutoPeças")
-        
+
         self.assertEqual(pontos_do_banco[1], ponto_2)
         self.assertEqual(pontos_do_banco[1].name, "Rodoviária")
 
-# Fucnoalidade 3
+    # Fucnoalidade 3
     def test_CT_03_str_representation(self):
         point = BoardingPoint.objects.create(name="Praça", route_order=5)
         self.assertEqual(str(point), "5: Praça")
