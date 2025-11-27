d4e7179ce5e512a4dd221526e34c612b376463eb
Vinícius Dias <vyvy292004@gmail.com>
2025-11-27 01:38:31 -0300

    Adds the function to generate weekly polls.

--- back-end/polls/management/commands/create_weekly_polls.py
+++ back-end/polls/management/commands/create_weekly_polls.py
@@ -1,17 +1,26 @@
 from django.core.management.base import BaseCommand
-from ...models import Poll
-from datetime import date, timedelta
+from django.utils import timezone
+from polls.models import Poll
+from datetime import timedelta
 
 
 class Command(BaseCommand):
-    help = "Create polls for next week (Monday to Friday)"
+    help = "Create polls for next week (Monday to Friday) and remove old polls"
 
     def handle(self, *args, **kwargs):
-        today = date.today()
+        today = timezone.localtime(timezone.now()).date()
+        
+        if today.weekday() != 5:
+            self.stdout.write(
+                self.style.WARNING("This command should only run on Saturdays")
+            )
+            return
+
         next_monday = today + timedelta(days=2)
-
+        
+        Poll.objects.filter(date__lt=next_monday).delete()
+        
         created_polls = []
-
         for i in range(5):
             poll_date = next_monday + timedelta(days=i)
             poll, created = Poll.objects.get_or_create(
@@ -23,8 +32,10 @@ class Command(BaseCommand):
         if created_polls:
             self.stdout.write(
                 self.style.SUCCESS(
-                    f"Created polls for next week: {', '.join(created_polls)}"
+                    f"Created polls for dates: {', '.join(created_polls)}"
                 )
             )
         else:
-            self.stdout.write("Polls for next week already exist.")
+            self.stdout.write(
+                self.style.SUCCESS("All polls for next week already exist")
+            )
