167147ec8b1a7e999032de69ae6964d878a118c6
Vinícius Dias <vyvy292004@gmail.com>
2025-11-27 01:24:34 -0300

    Refactor: removes dead code

--- back-end/students/serializers.py
+++ back-end/students/serializers.py
@@ -31,32 +31,7 @@ class StudentCreateSerializer(PersonSerializer):
         student = Student.objects.create(user=user, **validated_data)
         return student
 
-    def validate_phone(self, value):
-        if not re.fullmatch(r"\d{10,11}", value):
-            raise serializers.ValidationError(
-                "Phone number must contain 10 or 11 digits"
-            )
-        return value
-
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("This email is already in use")
-        return value
-
-    def validate_password(self, value):
-        if len(value) < 8:
-            raise serializers.ValidationError(
-                "Password must be at least 8 characters long"
-            )
-        if not re.search(r"\d", value):
-            raise serializers.ValidationError(
-                "Password must contain at least one number"
-            )
-        if not re.search(r"[A-Za-z]", value):
-            raise serializers.ValidationError(
-                "Password must contain at least one letter"
-            )
-        return value
+    
 
 
 class StudentSerializer(serializers.ModelSerializer):
