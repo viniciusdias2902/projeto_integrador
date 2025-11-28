254dea1ee7c8c1e65f1942a286fb8e3178e013b3
Vinícius Dias <vyvy292004@gmail.com>
2025-11-28 09:05:52 -0300

    feat: adds responsiveness to the navbar

--- front-end/src/components/NavBar.vue
+++ front-end/src/components/NavBar.vue
@@ -7,9 +7,6 @@ const route = useRoute()
 const { isStudent, isDriver, isAuthenticated, userRole } = useAuth()
 
 const authRoutes = ['/', '/cadastro']
-const studentRoutes = ['/enquetes', '/lista-embarque', '/acompanhar-viagem']
-const driverRoutes = ['/viagens', '/lista-embarque']
-const adminRoutes = ['/admin/estudantes']
 
 const isAdmin = computed(() => {
   const role = (userRole.value || '').toLowerCase()
@@ -20,9 +17,10 @@ const isAdmin = computed(() => {
 <template>
   <div class="navbar bg-base-100 shadow-sm">
     <div class="flex-1">
-      <a class="btn btn-ghost text-xl"> <RouterLink to="/">UniBus</RouterLink> </a>
+      <RouterLink to="/" class="btn btn-ghost text-xl">UniBus</RouterLink>
     </div>
-    <div class="flex-none">
+
+    <div class="flex-none hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
         <template v-if="authRoutes.includes(route.path)">
           <li>
@@ -110,6 +108,117 @@ const isAdmin = computed(() => {
           </li>
         </template>
       </ul>
+    </div>
+
+    <div class="flex-none lg:hidden">
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </div>
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+        >
+          <template v-if="authRoutes.includes(route.path)">
+            <li>
+              <RouterLink to="/">Login</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/cadastro">Cadastro</RouterLink>
+            </li>
+          </template>
+
+          <template v-else-if="isStudent && isAuthenticated">
+            <li>
+              <RouterLink to="/enquetes">Enquetes</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/acompanhar-viagem">Acompanhar Viagem</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/lista-embarque">Lista de embarque</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/" @click="logout">Sair</RouterLink>
+            </li>
+          </template>
+
+          <template v-else-if="isDriver && isAuthenticated">
+            <li>
+              <RouterLink to="/viagens">Viagens</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/lista-embarque">Lista de embarque</RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/" @click="logout">Sair</RouterLink>
+            </li>
+          </template>
+
+          <template v-else-if="isAdmin && isAuthenticated">
+            <li>
+              <RouterLink to="/admin/estudantes">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                </svg>
+                Estudantes
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/admin/pontos-embarque">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+                Pontos
+              </RouterLink>
+            </li>
+            <li>
+              <RouterLink to="/" @click="logout">Sair</RouterLink>
+            </li>
+          </template>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
